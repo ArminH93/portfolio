feb68f360f11c094c22437d9fb258b430815ef36
Armin Halilovic <ar.halilovic@gmail.com>
2023-01-02 20:53:28 +0100

    first commit

---
+++ pages/cv.tsx
@@ -0,0 +1,30 @@
+import { Inter } from '@next/font/google'
+import styles from '../styles/Home.module.css'
+import Head from 'next/head'
+import Link from 'next/link'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function CV() {
+  return (
+    <>
+      <Head>
+        <title>Porftolio</title>
+        <meta name="description" content="Mein Online Portfolio" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <h1 className={inter.className}>CV</h1>
+          <h2 className={inter.className}>
+            <Link href='/'>
+              Go back home
+            </Link>
+
+          </h2>
+        </div>
+      </main>
+    </>
+  )
+}
